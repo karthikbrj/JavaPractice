6ad9f5f082a5d6d1d7f46da7b450c321b2650f16
karthik <karthikbncet@gmail.com>
2024-06-12 12:28:45 +0530

    just small change

--- src/RemoveDuplicatesInLinkedList.java
+++ src/RemoveDuplicatesInLinkedList.java
@@ -6,6 +6,25 @@ public class RemoveDuplicatesInLinkedList {
     // input------>1->1->2->3->3
     // output---->1->2->3
 
+    // public class ListNode{
+    //     int val;
+    //     ListNode next;
+    //     ListNode(int val){
+    //         this.val=val;
+    //     }
+    //     ListNode(int val,ListNode next){
+    //         this.val=val;
+    //         this.next=next;
+    //     }
+        public static void main(String []args){
+            int arr[]={1,1,2,3,3};
+        }
+
+        
+
+
+
+    }
     
 
 
