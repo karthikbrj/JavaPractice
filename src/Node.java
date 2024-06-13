11e9c2b9422026d12bebca30a277862d8847099d
karthik <karthikbncet@gmail.com>
2024-06-13 12:21:10 +0530

    Node Class for Singlylinkedlist implementation

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+class Node{
+        int val;
+        Node next;
+        Node(int val){
+            this.val=val;
+        }
+        Node(int val,Node next){
+            this.val=val;
+            this.next=next;
+        }
+    }
