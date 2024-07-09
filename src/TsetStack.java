e31820bb9e99e79a1b59a4c1a8c58ebbccac9a86
karthik <karthikbncet@gmail.com>
2024-07-09 20:32:26 +0530

    addBinary

---
+++ src/TsetStack.java
@@ -0,0 +1,13 @@
+public class TsetStack {
+    public static void main(String[] args) {
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.print();
+        // System.err.println(s.pop());
+        // s.peek(); 
+    }
+}
