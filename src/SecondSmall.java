807a6410f367b61850b5fe9f69a27e1dc36e2e21
karthik <karthikbncet@gmail.com>
2024-07-27 23:26:21 +0530

    Finding second small elements in an array

---
+++ src/SecondSmall.java
@@ -0,0 +1,22 @@
+public class SecondSmall {
+    public static int findSecondLargestNumber(int []arr){
+        int largestNumber=Integer.MAX_VALUE;
+        int secondLargestNumber=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<largestNumber){
+                secondLargestNumber=largestNumber;
+                largestNumber=arr[i];
+            }
+            if(arr[i]<secondLargestNumber && arr[i]!=largestNumber){
+                secondLargestNumber=arr[i];
+            }
+        } 
+        return secondLargestNumber;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,10,35,9,7};
+       int secondLargestNumber= findSecondLargestNumber(arr);
+       System.out.println("The second largest number is "+secondLargestNumber);
+    }
+}
+
